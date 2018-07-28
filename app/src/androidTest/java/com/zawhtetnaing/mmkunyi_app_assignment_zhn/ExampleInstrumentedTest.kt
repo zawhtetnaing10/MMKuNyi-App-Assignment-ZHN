95d3af8c84addf7047e1f5c9c8b00c0ab095e177
zawhtetnaing10 <zawgyi.gog@gmail.com>
2018-07-28 08:32:14 +0630

    Project Skeleton with dependencies implemented

---
+++ app/src/androidTest/java/com/zawhtetnaing/mmkunyi_app_assignment_zhn/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.zawhtetnaing.mmkunyi_app_assignment_zhn
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.zawhtetnaing.mmkunyi_app_assignment_zhn", appContext.packageName)
+    }
+}
---
+++ app/src/main/java/com/zawhtetnaing/mmkunyi_app_assignment_zhn/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.zawhtetnaing.mmkunyi_app_assignment_zhn
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
